867b8fbd7c595d5b6da5aea62468e20143866a44
DoloressD <sarah.omar.a.a@gmail.com>
2022-07-04 11:57:00 -0700

    First commit

---
+++ Assets/GridController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridController : MonoBehaviour
+{
+    public Text[] gridSpots;
+    public List<string> syllables = new List<string>();
+
+
+    private void Start()
+    {
+        ShuffleGrid();
+        AddToGrid();
+    }
+    void ShuffleGrid()
+    {
+        System.Random rnd = new System.Random();
+        List<string> shuffledList = new List<string>();
+        int randomIndex = 0;
+
+        while(syllables.Count>0)
+        {
+            randomIndex = rnd.Next(0, syllables.Count);
+            shuffledList.Add(syllables[randomIndex]);
+            syllables.RemoveAt(randomIndex);
+
+        }
+        syllables = shuffledList;
+    }
+
+    void AddToGrid()
+    {
+        for (int i = 0; i < syllables.Count; i++)
+        {
+            gridSpots[i].text = syllables[i];
+        }
+    }
+
+}
---
+++ Assets/WordsController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordsController : MonoBehaviour
+{
+    public Text [] words;
+    public Text guessDisplay;
+    public GameObject winPanel;
+    public static string playerGuess = "";
+    private List<Text> lastGuess = new List<Text>();
+    private int correctGuesses = 0;
+
+    public void SetPlayerGuess(Text thisText)
+    {
+        if (playerGuess.Length < 15)
+        {
+            lastGuess.Add(thisText);
+            thisText.gameObject.SetActive(false);
+            playerGuess += thisText.text;
+            guessDisplay.text = playerGuess;
+            CheckWord();
+        }
+    }
+
+    public void RemoveLastGuess()
+    {
+        if(lastGuess.Count>0)
+        {
+            int lastGuessIndex = lastGuess.Count - 1;
+            playerGuess = playerGuess.Replace(lastGuess[lastGuessIndex].text, "");
+            lastGuess[lastGuessIndex].gameObject.SetActive(true);
+            lastGuess.RemoveAt(lastGuessIndex);
+            guessDisplay.text = playerGuess;
+        }
+    }
+
+    private void CheckWord()
+    {
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (string.Equals(words[i].text, playerGuess, System.StringComparison.OrdinalIgnoreCase))
+            {
+                words[i].gameObject.SetActive(true);
+                ResetGuess();
+                correctGuesses++;
+                CheckWin();
+            }
+        }
+    }
+
+    private void ResetGuess()
+    {
+        playerGuess = "";
+        guessDisplay.text = playerGuess;
+        lastGuess.Clear();       
+    }
+
+    private void CheckWin()
+    {
+        if(correctGuesses == 7)
+        {
+            winPanel.SetActive(true);
+        }
+    }
+
+}
