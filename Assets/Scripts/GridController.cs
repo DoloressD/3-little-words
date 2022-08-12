2d18b869f9df77a427476226ca8705e2294c5ae9
DoloressD <sarah.omar.a.a@gmail.com>
2022-08-11 17:16:01 -0700

    Added Restart Button, Changed Shuffling

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GridController : MonoBehaviour
+{
+    public Text[] gridSpots;
+    public List<string> syllables = new List<string>();
+
+
+    private void Start()
+    {
+        ShuffleGrid();
+        AddToGrid();
+    }
+    void ShuffleGrid()
+    {
+        System.Random rnd = new System.Random();
+        int randomIndex = 0;
+        string tempSyllable = "";
+
+        for(int i=0; i<syllables.Count-1; i++)
+		{
+            randomIndex = rnd.Next(i+1, syllables.Count);
+            tempSyllable = syllables[i];
+            syllables[i] = syllables[randomIndex];
+            syllables[randomIndex] = tempSyllable;
+		}
+    }
+
+    void AddToGrid()
+    {
+        for (int i = 0; i < syllables.Count; i++)
+        {  
+            gridSpots[i].text = syllables[i].ToUpper();
+
+        }
+    }
+
+}
